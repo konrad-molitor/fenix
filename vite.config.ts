6a9115b1f2330b146e72c17dbc924a1556b28bad
Ismail Valiev <ismail.valiev@dualbootpartners.com>
2025-09-20 18:37:20 -0300

    fix: vite alias

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,17 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import path from 'node:path';
 
 export default defineConfig(({ mode }) => {
     const isProd = mode === 'production';
 
     return {
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'resources/js'),
+            },
+        },
         plugins: [
             laravel({
                 input: ['resources/css/app.css', 'resources/js/app.tsx'],
